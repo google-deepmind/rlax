56080c4b66d10d6eace357cfacb3801954cc4dd5
joaogui1 <joaoguilhermearujo@gmail.com>
2020-04-27 09:05:32 -0300

    standardize example

--- examples/simple_dqn.py
+++ examples/simple_dqn.py
@@ -1,5 +1,7 @@
+"""A simple DQN agent trained to play cartpole."""
+from absl import app
+from absl import flags
 import jax
-from jax import jit, grad, vmap
 import jax.numpy as jnp
 from jax.experimental import optix
 import haiku as hk
@@ -15,14 +17,14 @@ from typing import Callable, Mapping, NamedTuple, Tuple, Sequence
 import matplotlib.pyplot as plt
 
 #Hyperparameters
-NUM_EPISODES = 5000
-BATCH_SIZE   = 32
-GAMMA        = 0.99
-LEARNING_RATE = 1e-3
-SEED = 1729 
+FLAGS = flags.FLAGS
+flags.DEFINE_integer("num_episodes", 5000, "Number of train episodes.")
+flags.DEFINE_integer("batch_size", 32, "Size of the training batch")
+flags.DEFINE_float("discount_factor", 0.99, "Q-learning discount factor.")
+flags.DEFINE_float("learning_rate", 1e-3, "Optimizer learning rate.")
+flags.DEFINE_integer("seed", 1729, "Random seed.")
 
 def plot(frame_idx, rewards, losses):
-    clear_output(True)
     plt.figure(figsize=(20,5))
     plt.subplot(131)
     plt.title('frame %s. reward: %s' % (frame_idx, np.mean(rewards[-10:])))
@@ -46,7 +48,7 @@ class ReplayBuffer(object):
         state, action, reward, next_state, done = zip(*random.sample(self.buffer, batch_size))
         return (jnp.concatenate(state), jnp.concatenate(next_state), 
                 jnp.asarray(action), jnp.asarray(reward), 
-                (1.-jnp.asarray(done, dtype=jnp.float32))*GAMMA)
+                (1.-jnp.asarray(done, dtype=jnp.float32))*FLAGS.discount_factor)
     
     def __len__(self):
         return len(self.buffer)
@@ -61,7 +63,7 @@ def build_network(num_actions: int) -> hk.Transformed:
 
   return hk.transform(q)
 
-def main():
+def main_loop(unused_args):
   # Build env
   env_id = "CartPole-v0"
   env = gym.make(env_id)
@@ -71,8 +73,9 @@ def main():
   epsilon_start = 1.0
   epsilon_final = 0.01
   epsilon_decay = 500
-
-  epsilon_by_frame = lambda frame_idx: epsilon_final + (epsilon_start - epsilon_final) * jnp.exp(-1. * frame_idx / epsilon_decay)
+  def epsilon_by_frame(frame_idx):
+    return epsilon_final + (epsilon_start - \
+      epsilon_final) * jnp.exp(-1. * frame_idx / epsilon_decay)
 
   #logging
   losses = []
@@ -83,12 +86,12 @@ def main():
   num_actions = env.action_space.n
   network = build_network(num_actions)
   sample_input = env.reset()
-  rng = hk.PRNGSequence(jax.random.PRNGKey(SEED))
+  rng = hk.PRNGSequence(jax.random.PRNGKey(FLAGS.seed))
   net_params = network.init(next(rng), sample_input)
   target_params = net_params
 
    # Build and initialize optimizer.
-  optimizer = optix.adam(LEARNING_RATE)
+  optimizer = optix.adam(FLAGS.learning_rate)
   opt_state = optimizer.init(net_params) 
 
   @jax.jit
@@ -104,7 +107,7 @@ def main():
     q = network.apply(net_params, obs)
     return rlax.greedy().sample(key, q)
 
-  batched_loss = vmap(rlax.double_q_learning)
+  batched_loss = jax.vmap(rlax.double_q_learning)
   @jax.jit
   def update(net_params, target_params, opt_state, batch):
     """Update network weights wrt Q-learning loss."""
@@ -126,8 +129,8 @@ def main():
     return net_params, opt_state, loss
 
   state = env.reset()
-  print(f"Training agent for {NUM_EPISODES} episodes...")
-  for idx in range(1, NUM_EPISODES+1):
+  print(f"Training agent for {FLAGS.num_episodes} episodes...")
+  for idx in range(1, FLAGS.num_episodes+1):
     epsilon = epsilon_by_frame(idx)
 
     #Act in the environment and update replay_buffer
@@ -144,16 +147,19 @@ def main():
         all_rewards.append(episode_reward)
         episode_reward = 0
         
-    if len(replay_buffer) > BATCH_SIZE:
-      batch = replay_buffer.sample(BATCH_SIZE)
+    if len(replay_buffer) > FLAGS.batch_size:
+      batch = replay_buffer.sample(FLAGS.batch_size)
       net_params, opt_state, loss = update(net_params, target_params, 
                                            opt_state, batch)
       losses.append(float(loss))
-        
-    if idx % 200 == 0:
-     plot(idx, all_rewards, losses)
-      
+
     #Update Target model parameters
     if idx % 100 == 0:
       target_params = net_params
-main()
+
+    if idx % 200 == 0:
+      print("Average rewards:", np.mean(all_rewards[-10:]))
+
+  plot(idx, all_rewards, losses)
+if __name__ == "__main__":
+  app.run(main_loop)

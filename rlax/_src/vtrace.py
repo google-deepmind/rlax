e5487cad2358c86ffc9b4071e340bc65c466e321
RLaxDev <rlax-dev@google.com>
2020-11-13 02:48:23 -0800

    Adding an implementation for leaky_vtrace_td_error_and_advantage
    PiperOrigin-RevId: 342227781
    Change-Id: I2973215846242283f4f5fe696197c7a3c5608dd1

--- rlax/_src/vtrace.py
+++ rlax/_src/vtrace.py
@@ -212,3 +212,58 @@ def vtrace_td_error_and_advantage(
   pg_advantages = clipped_pg_rho_tm1 * (q_estimate - v_tm1)
   return VTraceOutput(
       errors=errors, pg_advantage=pg_advantages, q_estimate=q_estimate)
+
+
+def leaky_vtrace_td_error_and_advantage(
+    v_tm1: chex.Array,
+    v_t: chex.Array,
+    r_t: chex.Array,
+    discount_t: chex.Array,
+    rho_t: chex.Array,
+    alpha: float = 1.0,
+    lambda_: float = 1.0,
+    clip_rho_threshold: float = 1.0,
+    clip_pg_rho_threshold: float = 1.0,
+    stop_target_gradients: bool = True,
+) -> VTraceOutput:
+  """Calculates V-Trace errors and PG advantage from importance weights.
+
+  This functions computes the TD-errors and policy gradient Advantage terms
+  as used by the IMPALA distributed actor-critic agent.
+
+  See "IMPALA: Scalable Distributed Deep-RL with Importance Weighted Actor
+  Learner Architectures" by Espeholt et al. (https://arxiv.org/abs/1802.01561)
+
+  Args:
+    v_tm1: values at time t-1.
+    v_t: values at time t.
+    r_t: reward at time t.
+    discount_t: discount at time t.
+    rho_t: importance weights at time t.
+    alpha: mixing the clipped importance sampling weights with unclipped ones.
+    lambda_: scalar mixing parameter lambda.
+    clip_rho_threshold: clip threshold for importance ratios.
+    clip_pg_rho_threshold: clip threshold for policy gradient importance ratios.
+    stop_target_gradients: whether or not to apply stop gradient to targets.
+
+  Returns:
+    a tuple of V-Trace error, policy gradient advantage, and estimated Q-values.
+  """
+  chex.assert_rank([v_tm1, v_t, r_t, discount_t, rho_t], 1)
+  chex.assert_type([v_tm1, v_t, r_t, discount_t, rho_t], float)
+  chex.assert_equal_shape([v_tm1, v_t, r_t, discount_t, rho_t])
+
+  errors = leaky_vtrace(
+      v_tm1, v_t, r_t, discount_t, rho_t, alpha,
+      lambda_, clip_rho_threshold, stop_target_gradients)
+  targets_tm1 = errors + v_tm1
+  q_bootstrap = jnp.concatenate([
+      lambda_ * targets_tm1[1:] + (1 - lambda_) * v_tm1[1:],
+      v_t[-1:],
+  ], axis=0)
+  q_estimate = r_t + discount_t * q_bootstrap
+  clipped_pg_rho_tm1 = (
+      (1 - alpha) * rho_t + alpha * jnp.minimum(clip_pg_rho_threshold, rho_t))
+  pg_advantages = clipped_pg_rho_tm1 * (q_estimate - v_tm1)
+  return VTraceOutput(
+      errors=errors, pg_advantage=pg_advantages, q_estimate=q_estimate)

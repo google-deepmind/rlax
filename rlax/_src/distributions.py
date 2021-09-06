Fixes bug in kl calculation of gaussian_diagonal
The code assumed that distrax returns a tuple when calculating the kl-divergence,
which is not the case.
User-code should have thrown an error when attempting to use this function, unless
the function was applied to data with a leading batch-dim of size 2.

PiperOrigin-RevId: 395095152

rlax/_src/distributions.py
@@ -228,11 +228,10 @@ def gaussian_diagonal(sigma=None):
         mu, jnp.ones_like(mu) * sigma).entropy()
 
   def kl_to_standard_normal_fn(mu: Array, sigma: Array = sigma):
-    kl_mean, kl_cov = distrax.MultivariateNormalDiag(
+    return distrax.MultivariateNormalDiag(
         mu, jnp.ones_like(mu) * sigma).kl_divergence(
             distrax.MultivariateNormalDiag(
                 jnp.zeros_like(mu), jnp.ones_like(mu)))
-    return kl_mean + kl_cov
 
   def kl_fn(mu_1: Array, sigma_1: Numeric, mu_0: Array, sigma_0: Numeric):
     return distrax.MultivariateNormalDiag(

rlax/_src/distributions_test.py
@@ -411,7 +411,7 @@ class GaussianDiagonalTest(parameterized.TestCase):
     self.expected_kl = np.array(
         [4050.00, 1.00435], dtype=np.float32)
     self.expected_kl_to_std_normal = np.array(
-        4.6151705 + 1.8884108, dtype=np.float32)
+        [4.6151705, 1.8884108], dtype=np.float32)
 
   @chex.all_variants()
   def test_gaussian_prob(self):
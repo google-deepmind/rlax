0fed41c70a30945ce2d39571f9d65009e1bb45f3
Iurii Kemaev <iukemaev@google.com>
2025-09-01 16:38:19 -0700

    release v0.1.8
    PiperOrigin-RevId: 801917589

--- rlax/__init__.py
+++ rlax/__init__.py
@@ -142,7 +142,7 @@ from rlax._src.vtrace import leaky_vtrace_td_error_and_advantage
 from rlax._src.vtrace import vtrace
 from rlax._src.vtrace import vtrace_td_error_and_advantage
 
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 
 __all__ = (
     "add_gaussian_noise",
